7ade37e33071c59b7c8974493271de4402ddbeab
Shukla600 <78774642+Shukla600@users.noreply.github.com>
2022-12-28 17:27:43 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+	$(".slider-one").owlCarousel({
+		items:1,
+		autoplay:true,
+		autoplayTimeout:5000,
+		loop:true,
+		
+		
+	});   
+  });
+  $(document).ready(function(){
+	$(".slider-two").owlCarousel({
+		items:3,
+		autoplay:true,
+		autoplayTimeout:5000,
+		loop:true,
+		nav:true,
+		navText:['next','previous'],
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1,
+				nav:true
+			},
+			600:{
+				items:2,
+				nav:false
+			},
+			
+		}
+	});   
+  });
+  
+  $(document).ready(function(){
+	$(".slider-three").owlCarousel({
+		items:1,
+		autoplay:true,
+		autoplayTimeout:5000,
+		loop:true,
+	});   
+  });
+
+  $(document).ready(function(){
+	$(".slider-four").owlCarousel({
+		items:1,
+		autoplay:true,
+		autoplayTimeout:5000,
+		loop:true,
+	});   
+  });
+
+
+  
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
